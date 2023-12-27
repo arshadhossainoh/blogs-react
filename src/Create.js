e7d65d515c3aaa1bca78da114c89a7433602a2c7
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-12-27 11:28:28 -0500

    worked on deleting blog button from blog list

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("karl");
   const [isPending, setisPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +20,7 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setisPending(false);
+      history.push("/");
     });
   };
 
